a09d792e8e20a7a7da60226bf185c7c0340a9420
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-11-10 07:57:10 -0500

    Limpieza de código

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -231,51 +231,6 @@ func init() {
 			Filters:          nil,
 			Params:           nil})
 
-	beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"],
-		beego.ControllerComments{
-			Method:           "Post",
-			Router:           "/",
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"],
-		beego.ControllerComments{
-			Method:           "GetAll",
-			Router:           "/",
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"],
-		beego.ControllerComments{
-			Method:           "GetOne",
-			Router:           "/:id",
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"],
-		beego.ControllerComments{
-			Method:           "Put",
-			Router:           "/:id",
-			AllowHTTPMethods: []string{"put"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:TipoAntecedenteController"],
-		beego.ControllerComments{
-			Method:           "Delete",
-			Router:           "/:id",
-			AllowHTTPMethods: []string{"delete"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
 	beego.GlobalControllerRouter["Psicologia/controllers:ValoracionInterpersonalController"] = append(beego.GlobalControllerRouter["Psicologia/controllers:ValoracionInterpersonalController"],
 		beego.ControllerComments{
 			Method:           "Post",
